eb5f7191d162df7830de68c37a42558e96b493ce
Fleshborne <blakeathompson94@gmail.com>
2020-06-20 00:53:05 -0400

    schema created

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,28 @@ CREATE DATABASE employees;
 USE employees;
 
 -- create a departments table
-
+CREATE TABLE departments (
+ id INT NOT NULL AUTO_INCREMENT,
+ department_name VARCHAR(30) NOT NULL,   
+ PRIMARY KEY (id)
+ 
+);
 -- create a roles table
+CREATE TABLE roles (
+ id INT NOT NULL AUTO_INCREMENT,
+ title VARCHAR(30) NOT NULL,
+ salary DECIMAL(10,2),
+ department_id INT NOT NULL,
+ PRIMARY KEY(id)
+)
+
+CREATE TABLE employees (
+ id INT NOT NULL AUTO_INCREMENT,
+ first_name VARCHAR(30) NOT NULL,
+ last_name VARCHAR(30) NOT NULL,
+ role_id INT NOT NULL,
+ manager_id INT,
+ PRIMARY KEY(id)
+)
 
 -- create an employees table
