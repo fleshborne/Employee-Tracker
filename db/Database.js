d957e4f19c7d9f55928118283cb33e214ef19059
Fleshborne <blakeathompson94@gmail.com>
2020-06-22 11:48:45 -0400

    database.js complete aside delete function, will come back to that, working on index.js next

--- db/Database.js
+++ db/Database.js
@@ -4,47 +4,50 @@ class Database {
   constructor(connection) {
     this.connection = connection;
   }
-  createDepartment() {
-    return this.connection
-      .query
+  createDepartment(department) {
+    return this.connection.query(
       // SELECT query here
-      ();
+      "INSERT INTO department SET ?",
+      department
+    );
   }
-  createEmployee() {
-    return this.connection
-      .query
+  createEmployee(employee) {
+    return this.connection.query(
       // SELECT query here
-      ();
+      "INSERT INTO employee SET ?",
+      employee
+    );
   }
-  createRole() {
-    return this.connection
-      .query
+  createRole(roles) {
+    return this.connection.query(
       // SELECT query here
-      ();
+      "INSERT INTO roles SET ?",
+      roles
+    );
   }
   getDepartment() {
-    return this.connection
-      .query
+    return this.connection.query(
       // SELECT query here
-      ();
+      "SELECT * FROM department"
+    );
   }
   getEmployee() {
-    return this.connection
-      .query
+    return this.connection.query(
       // SELECT query here
-      ();
+      "SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS department, roles.salary FROM employee LEFT JOIN roles on employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id;"
+    );
   }
   getRole() {
-    return this.connection
-      .query
+    return this.connection.query(
       // SELECT query here
-      ();
+      "SELECT roles.id, roles.title, department.name AS department, roles.salary FROM roles LEFT JOIN department ON roles.department_id = department.id"
+    );
   }
-  updateRole() {
-    return this.connection.query();
-  }
-  updateManagers() {
-    return this.connection.query();
+  updateRole(employeeId, roleId) {
+    return this.connection.query(
+      "UPDATE employee SET role_id = ? WHERE ID = ?",
+      [employeeId, roleId]
+    );
   }
   deleteEmployee() {
     return this.connection.query();
